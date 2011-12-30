7c28fe6bcc60959a84f7d2cd6aa3aff11bdba38c
Simon Hengel <sol@typeful.net>
2011-12-30 10:00:47 +0100

    Refactoring of Spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,24 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind -fno-warn-missing-signatures #-}
 module Spec where
-import Test.Hspec.Monadic
-import Test.Hspec.QuickCheck
-import Test.Hspec.HUnit
-import Test.QuickCheck hiding (property)
-import Test.HUnit
 
-import Control.Monad
+import Test.Hspec.ShouldBe
 
 import Data.String.Builder
-
-shouldBe :: (Show a, Eq a) => a -> a -> Assertion
-actual `shouldBe` expected = unless (actual == expected) (assertFailure message)
-  where message = show actual ++ " was not equal to " ++ show expected
 
 main = hspec $ do
   describe "build" $ do
     it "can be used to construct multi-line string literals in a monadic way" $ do
-      let mystring = build $ do
-          "foo"
-          "bar"
-          "baz"
-      mystring `shouldBe` "foo\nbar\nbaz\n"
+      build $ do
+        "foo"
+        "bar"
+        "baz"
+      `shouldBe` "foo\nbar\nbaz\n"
