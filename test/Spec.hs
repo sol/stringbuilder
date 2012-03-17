d56d79bc75b7945b7acf4dea9b0f920d116fa24b
Simon Hengel <sol@typeful.net>
2012-03-17 07:09:38 +0100

    Add more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -fno-warn-unused-do-bind -fno-warn-missing-signatures #-}
-module Spec where
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+module Main (main, spec) where
 
-import Test.Hspec.ShouldBe
+import           Test.Hspec.ShouldBe
 
-import Data.String.Builder
+import           Data.String
+import           Data.String.Builder
 
-main = hspec $ do
+main :: IO ()
+main = hspecX spec
+
+spec :: Specs
+spec = do
   describe "build" $ do
     it "can be used to construct multi-line string literals in a monadic way" $ do
       build $ do
@@ -14,3 +19,6 @@ main = hspec $ do
         "bar"
         "baz"
       `shouldBe` "foo\nbar\nbaz\n"
+
+    prop "works for arbitrary input" $ do
+      \l -> (build . sequence_ . map fromString) l == unlines l
