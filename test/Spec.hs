05409887504075920fdbb1b3ba8f18a3ecfeacdb
Simon Hengel <sol@typeful.net>
2012-07-13 18:07:00 +0200

    Use hspec-1.3

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,15 +2,16 @@
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 module Main (main, spec) where
 
-import           Test.Hspec.ShouldBe
+import           Test.Hspec
+import           Test.QuickCheck
 
 import           Data.String
 import           Data.String.Builder
 
 main :: IO ()
-main = hspecX spec
+main = hspec spec
 
-spec :: Specs
+spec :: Spec
 spec = do
   describe "build" $ do
     it "can be used to construct multi-line string literals in a monadic way" $ do
@@ -20,5 +21,6 @@ spec = do
         "baz"
       `shouldBe` "foo\nbar\nbaz\n"
 
-    prop "works for arbitrary input" $ do
-      \l -> (build . sequence_ . map fromString) l == unlines l
+    it "works for arbitrary input" $ do
+      property $
+        \l -> (build . sequence_ . map fromString) l == unlines l
